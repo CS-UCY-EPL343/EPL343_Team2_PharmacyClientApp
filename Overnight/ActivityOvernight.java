54354d393d0cad67fd1035a32a417a052c55f7fc
ivalia01 <43334790+ivalia01@users.noreply.github.com>
2018-11-26 12:15:37 +0200

    Add files via upload

---
+++ Overnight/ActivityOvernight.java
@@ -0,0 +1,13 @@
+package com.example.ioann.dianikterevonta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityOvernight extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dianikterevonta);
+    }
+}
